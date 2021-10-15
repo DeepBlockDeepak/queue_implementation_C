be2cb70151a1d384a1159dbc7f68ef7c96c9ea0a
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-10-15 00:17:46 -0600

    creating the functions script which gets imported with header for the queue.c driver script

---
+++ functions.c
@@ -0,0 +1,12 @@
+/**
+ * @file		functions.c
+ * 
+ * @author		Jordan Medina (jordan.medina@student.nmt.edu)
+ * 
+ * @brief		Functions used in queue.c
+ * 
+ * @date		2021-10-15
+ * 
+ * 
+ */
+
---
+++ functions.h
@@ -0,0 +1,12 @@
+/**
+ * @file		functions.h
+ * 
+ * @author		Jordan Medina (jordan.medina@student.nmt.edu)
+ * 
+ * @brief		Header file for importing the functions.c functions into the queue.c driver 
+ * 
+ * @date		2021-10-15
+ * 
+ * 
+ */
+
